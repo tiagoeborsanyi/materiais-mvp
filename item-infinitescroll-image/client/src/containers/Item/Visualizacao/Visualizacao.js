7c4ff117e6522413d3b9035df68160137d609ec7
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-03-20 18:35:21 -0300

    setatada as props pra visualizacao de item

--- item-infinitescroll-image/client/src/containers/Item/Visualizacao/Visualizacao.js
+++ item-infinitescroll-image/client/src/containers/Item/Visualizacao/Visualizacao.js
@@ -6,6 +6,11 @@ import classes from './Visualizacao.css';
 
 class Visualizacao extends Component {
 
+    state = {
+        item: {},
+        update: true
+    }
+
     deleteItem = (id) => {
         axios.delete(`/api/item/${id}`)
             .then(res => {
@@ -17,46 +22,66 @@ class Visualizacao extends Component {
             .catch(err => console.log(err));
     }
 
-  render() {
-      // console.log(this.props);
+    componentDidMount() {
+        console.log('didMount')
+        if (this.state.update) {
+            const id = this.props.location.hash.slice(1);
+            axios.get(`/api/item/${id}`)
+            .then(res => {
+                this.setState({item: res.data, update: false});
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+        }
+    }
+    componentDidUpdate() {
+        console.log('update');
+        
+    }
+
+
+    render() {
       const id = this.props.location.hash.slice(1);
-    return (
-      <div className={classes.Container}>
-        <div className={classes.image}>
-            <img src={require(`../../../assests/images/no-photo.png`)} alt="Interruptor de luz" className={classes.image__item} />
-            <div className={classes.image__text}>
-                <p>Qtd. Estoque</p>
-                <p>1080</p>
-                <p><Link to="/">Historico Estoque</Link></p>
-                <p><Link to="/">Historico Guias</Link></p>
+      //console.log(this.state.item)
+      const objItem = {...this.state.item};
+      console.log(objItem.model)
+        return (
+        <div className={classes.Container}>
+            <div className={classes.image}>
+                <img className={classes.image__item} src={this.state.item.imagem ? require(`../../../assests/${this.state.item.imagem}`) : require('../../../assests/images/no-photo.png')} alt="Interruptor de luz" />
+                <div className={classes.image__text}>
+                    <p>Qtd. Estoque</p>
+                    <p>1080</p>
+                    <p><Link to="/">Historico Estoque</Link></p>
+                    <p><Link to="/">Historico Guias</Link></p>
+                </div>
+            </div>
+            <div className={classes.content}>
+                <div className={classes.content__botao}>
+                    <button>Alterar Estoque</button>
+                    <Link to={{
+                    pathname: "/edita",
+                    hash: this.props.location.hash
+                    }}>Editar</Link>
+                    <button onClick={() => this.deleteItem(id)}>Excluir</button>
+                </div>
+                <p><span>Nome: </span>{this.state.item.name}</p>
+                <div className={classes.item__texto}>
+                    <p><span>Modelo: </span>{this.state.item.model}</p>
+                    <p><span>Marca: </span>{this.state.item.brand}</p>
+                </div>
+                <p><span>Especificação: </span>{this.state.item.especification}</p>
+                <p><span>Código referencia: </span>{this.state.item.codrefproduct}</p>
+                <div className={classes.item__texto}>
+                    <p><span>Tipo Embalagem: </span>{this.state.item.packingtype}</p>
+                    <p><span>Qtd. Embalagem: </span>{this.state.item.qtdpacking}</p>
+                </div>
+                <p><span>descrição: </span>{this.state.item.descrition}</p>
+                <p><span>Observação: </span>{this.state.item.observation}</p>
             </div>
         </div>
-        <div className={classes.content}>
-            <div className={classes.content__botao}>
-                <button>Alterar Estoque</button>
-                <Link to={{
-                  pathname: "/edita",
-                  hash: this.props.location.hash
-                }}>Editar</Link>
-                <button onClick={() => this.deleteItem(id)}>Excluir</button>
-            </div>
-            <p><span>Nome: </span>Conjunto Interruptor Simples</p>
-            <div className={classes.item__texto}>
-                <p><span>Modelo: </span>Placa 4x2 Duale Up Branco</p>
-                <p><span>Marca: </span>Iriel</p>
-            </div>
-            <p><span>Especificação: </span>5 x 3 x 4 cm (largura x comprimento x altura)</p>
-            <p><span>Código referencia: </span>615089</p>
-            <div className={classes.item__texto}>
-                <p><span>Tipo Embalagem: </span>Pacote</p>
-                <p><span>Qtd. Embalagem: </span>1</p>
-            </div>
-            <p><span>descrição: </span>O Conjunto Interruptor Simples com Placa 4x2 Duale UP Branco da Iriel apresenta uma estrutura sem parafusos aparentes, além de possuir um design inovador, superfície brilhante e toque suave. O produto é produzido em material termoplástico de alta performance com aditivo anti UV e anti poeira, que auxilia na sua conservação. </p>
-            <p><span>Observação: </span>A Iriel é uma empresa totalmente comprometida com a qualidade desde sua fundação, sendo a pioneira do setor elétrico na região sul do país a receber a Marca Nacional de Conformidade, concedida pelo Inmetro no ano de 1984. Marca concedida ao ser atestada a garantia de que todos os produtos de sua linha são seguros, têm qualidade e atendem às especificações técnicas.</p>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Visualizacao;
