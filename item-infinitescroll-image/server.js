14ed5cd8de875e3ccbb41165f9ac84f82d177d47
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-03-31 16:35:40 -0300

    pronto para rodar os certificado ssl

--- item-infinitescroll-image/server.js
+++ item-infinitescroll-image/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const http = require('http');
 const https = require('https');
 const fs    = require('fs');
+const path = require('path');
 
 const item = require('./routes/item');
 
@@ -13,11 +14,11 @@ const app = express();
 
 require('dotenv').config();
 
-const options = {
-    key: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/privkey1.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/fullchain1.pem"),
-    ca: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/chain1.pem")
-  };
+// const options = {
+//     key: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/privkey1.pem"),
+//     cert: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/fullchain1.pem"),
+//     ca: fs.readFileSync("/etc/letsencrypt/archive/evsoul.com/chain1.pem")
+//   };
 
 app.use('*', cors());
 
@@ -41,8 +42,8 @@ mongoose
 // rotas
 app.use('/api/item', item);
 
-const server = http.createServer(app);
-const httpsServer = https.createServer(options, app);
-
-server.listen(5000, () => console.log(`Server rodando na porta 5000`));
-httpsServer.listen(443, () => console.log(`Server rodando na porta 5000`));
+// const server = http.createServer(app);
+// const httpsServer = https.createServer(options, app);
+app.listen(5000, () => console.log(`Server rodando na porta 5000`));
+// server.listen(5000, () => console.log(`Server rodando na porta 5000`));
+// httpsServer.listen(443, () => console.log(`Server rodando na porta 5000`));
